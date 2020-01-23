ec355be2625913537be1b08f3a7e9827b7fe4aac
Erica Schimmel <ericaschimmel01@gmail.com>
2020-01-23 19:20:35 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -26,7 +26,10 @@ def monopoly_with_third_tier
 	
 	return monopoly = {:railroads => {
 	  :pieces => 4,
-	  :names => {}
+	  :names => {},
+	  :rent_in_dollars => {
+	      :one_piece_owned => 25
+	    }
 	  }
 	}
 end
